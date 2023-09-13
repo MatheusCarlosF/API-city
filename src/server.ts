67d013ba8105802a3d9b339f77cfeffc04576f58
MatheusCarlosF <matheuscarlos16@hotmail.com>
2023-09-13 20:38:01 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
-import express, {Request, Response} from "express"
+import express, { Request, Response } from "express"
 import path from "path"
 import dotenv from "dotenv"
 import apiRoutes from "./routes/api"
+var cors = require('cors')
+
 
 dotenv.config()
 const server = express()
 
-server.use(express.static(path.join(__dirname,'../public')))
-server.use(express.urlencoded({extended: true}))
+server.use(cors())
+server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.urlencoded({ extended: true }))
 
 server.use(apiRoutes)
 
 server.use((req: Request, res: Response) => {
     res.status(404)
-    res.json({error: "Endpoint not found"});
+    res.json({ error: "Endpoint not found" });
 })
 
 server.listen(process.env.PORT)
